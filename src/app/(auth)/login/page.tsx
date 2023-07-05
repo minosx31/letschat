82b8d1f8ff4cbd3c3f16eab70ce0253f8239d278
Mingz <n_minghao28@hotmail.com>
2023-07-05 22:53:18 +0800

    Add app logo and app name

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -4,6 +4,8 @@ import Button from '@/components/ui/Button'
 import { FC, useState } from 'react'
 import { signIn } from 'next-auth/react'
 import { toast } from 'react-hot-toast'
+import logo from '../../../../public/chat-100.png'
+import Image from 'next/image'
 
 const Page: FC = () => {
     const [isLoading, setIsLoading] = useState<boolean>(false)
@@ -22,11 +24,16 @@ const Page: FC = () => {
 
   return (
   <>
-    <div className='flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg: px-8'>
+    <div className='flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
         <div className='w-full flex flex-col items-center max-w-md space-y-8'>
+            <div className="flex flex-col items-center gap-4">
+                <Image src={logo} alt='app logo' className='h-30 w-30'/>
+                <h1 className='mb-6 text-center text-4xl font-bold tracking-wide'>LetsChat</h1>
+            </div>
+            
+
             <div className='flex flex-col items-center gap-8'>
-                LOGO
-                <h2 className='mt-6 text-center text-3xl font-bold tracking-tight text-gray-900'>Sign in to your account</h2>
+                <h3 className='mt-6 text-center text-2xl font-semibold tracking-tight text-gray-900'>Sign in to your account</h3>
 
                 <Button isLoading={isLoading} type='button' className='max-w-sm mx-auto w-full' onClick={loginWithGoogle}>
                     {isLoading ? null : (<svg
